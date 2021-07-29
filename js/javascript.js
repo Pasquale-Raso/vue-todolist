98fa28c5ba9ec2d39579bde1bcbbef95417e710f
Pasquale-Raso <82545398+Pasquale-Raso@users.noreply.github.com>
2021-07-29 21:32:58 +0200

    campo input-button aggiungi

--- js/javascript.js
+++ js/javascript.js
@@ -4,6 +4,7 @@ console.log("Ciao", Vue);
 const app = new Vue({
   el: "#app",
   data: {
+    newTask: "",
     tasks: [
       "Compra il latte",
       "Compra la carne",
@@ -11,5 +12,15 @@ const app = new Vue({
       "Compra i biscotti",
     ],
   },
-  methods: {},
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
+    },
+    addTask() {
+      if (this.newTask && this.newTask.trim() !== "") {
+        this.tasks.push(this.newTask);
+        this.newTask = "";
+      }
+    },
+  },
 });
